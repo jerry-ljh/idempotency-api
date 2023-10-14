2d7c6748e07a5608096b48f6823e30a65f3cebac
jerry <gurwns5580@gmail.com>
2023-10-14 15:18:51 +0900

    feat: aop, excutor를 적용한 게시글 생성 메서드 작성

--- src/main/kotlin/com/example/idempotentapi/configuration/CacheKeys.kt
+++ src/main/kotlin/com/example/idempotentapi/configuration/CacheKeys.kt
@@ -3,4 +3,5 @@ package com.example.idempotentapi.configuration
 object CacheKeys {
     const val IDEMPOTENCY_REQUEST_KEY = "idempotency.request"
     const val IDEMPOTENCY_RESPONSE_KEY = "idempotency.response"
+    const val POST_CREATE_KEY = "post.create"
 }
--- src/main/kotlin/com/example/idempotentapi/service/PostService.kt
+++ src/main/kotlin/com/example/idempotentapi/service/PostService.kt
@@ -1,15 +1,20 @@
 package com.example.idempotentapi.service
 
+import com.example.idempotentapi.component.IdempotencyExecutor
+import com.example.idempotentapi.configuration.CacheKeys.POST_CREATE_KEY
 import com.example.idempotentapi.controller.dto.CreatePostRequest
 import com.example.idempotentapi.controller.dto.PatchPostRequest
 import com.example.idempotentapi.domain.Post
 import com.example.idempotentapi.repository.PostJpaRepository
+import com.example.idempotentapi.util.Idempotency
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.lang.Thread.sleep
 
 @Service
 class PostService(
-    val postRepository: PostJpaRepository
+    private val postRepository: PostJpaRepository,
+    private val idempotencyExecutor: IdempotencyExecutor
 ) {
 
     private val log = LoggerFactory.getLogger(this::class.simpleName)
@@ -17,7 +22,18 @@ class PostService(
     fun createPost(postRequest: CreatePostRequest) {
         val postEntity = Post(userId = postRequest.userId, contents = postRequest.contents)
         postRepository.save(postEntity)
-        log.info("게시글이 포스팅 content: ${postEntity.contents}")
+        sleep(3000)
+        log.info("게시글 포스팅 content: ${postEntity.contents}")
+    }
+
+    fun createPostWithIdempotencyExecutor(postRequest: CreatePostRequest) {
+        val key = "$POST_CREATE_KEY::${postRequest.userId},${postRequest.contents.hashCode()}"
+        idempotencyExecutor.execute(key) { createPost(postRequest) }
+    }
+
+    @Idempotency(key = POST_CREATE_KEY, expression = "#postRequest.userId + ',' + #postRequest.contents.hashCode()")
+    fun createPostWithIdempotencyAop(postRequest: CreatePostRequest) {
+        createPost(postRequest)
     }
 
     fun updatePost(postRequest: PatchPostRequest) {
